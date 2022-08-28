5d3c132b2b0341d3eaad13b1dbc657d273dd223c
milana-kovacevic <kovacevicmilana95@yahoo.com>
2022-08-29 00:53:08 +0200

    Added test project for functional tests instead of ClientApp. This test will be used to test end to end functionality of the service.

---
+++ tests/FunctionalTests/EndToEndTest.cs
@@ -0,0 +1,31 @@
+using DistributedCalculationSystem;
+
+namespace FunctionalTests
+{
+    /// <summary>
+    /// This is end to end test scenario.
+    /// In order to run these tests, local setup must be created.
+    /// There should be:
+    ///     1. Frontend service running locally at http://host.docker.internal:8081
+    ///     2. ComputeNode service running at corresponding port (8080).
+    /// </summary>
+    public class EndToEndTest
+    {
+        [Fact]
+        public async Task ListAllJobs_Success()
+        {
+            string baseUrl = "http://host.docker.internal:8081";
+            var client = new DistributedCalculationSystemClient(baseUrl, new HttpClient());
+
+            var jobs = await client.AllAsync();
+
+            Console.WriteLine("Jobs:");
+            foreach (var job in jobs)
+            {
+                Console.WriteLine(job.ToString());
+            }
+
+            Assert.NotEmpty(jobs);
+        }
+    }
+}
---
+++ tests/FunctionalTests/TestConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalTests
+{
+    public static class TestConstants
+    {
+        public static readonly string ClientId = "51efcfc4-6aeb-4500-9440-15536a1acb97";
+        public static readonly string Secret = "<placeholder>";
+        public static readonly string TenantId = "059292d2-e768-40fb-a0a6-ab1e4639ea8c";
+        public static readonly string TargetClientId = "b09bd1fa-9254-4d4c-94dc-ad823bb48b9e";
+        public static readonly string TargetAudience = "api://frontend-webapi";
+        public static readonly string Issuer = "https://sts.windows.net/059292d2-e768-40fb-a0a6-ab1e4639ea8c/v2.0";
+        public static readonly string AuthServer = "https://login.microsoftonline.com/";
+    }
+}
