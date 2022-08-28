786ed1e15ba3f43ab634a964cb7acc12fbb7c066
milana-kovacevic <kovacevicmilana95@yahoo.com>
2022-08-29 01:31:55 +0200

    Creating ComputeNodeClientWrapper

---
+++ src/Frontend/ComputeNodeSwaggerClient/ComputeNodeClientWrapper.cs
@@ -0,0 +1,26 @@
+﻿using ComputeNodeSwaggerClient;
+using Frontend.Topology;
+
+namespace Frontend.ComputeNodeSwaggerClient
+{
+    public class ComputeNodeClientWrapper : IComputeNodeClientWrapper
+    {
+        private readonly IAddressManager _addressManager;
+        private readonly HttpClient _httpClient;
+        private readonly ComputeNodeClient _computeNodeClient;
+
+        public ComputeNodeClientWrapper(IAddressManager addressManager)
+        {
+            this._addressManager = addressManager;
+            this._httpClient = new HttpClient();
+
+            var computeNodeServiceAddress = _addressManager.ComputeNodeServiceAddress;
+            _computeNodeClient = new ComputeNodeClient(computeNodeServiceAddress, _httpClient);
+        }
+
+        public async Task<AtomicJobResult> RunAsync(int atomicJobId, int parentJobId, string inputData)
+        {
+            return await _computeNodeClient.RunAsync(atomicJobId, parentJobId, inputData);
+        }
+    }
+}
---
+++ src/Frontend/ComputeNodeSwaggerClient/IComputeNodeClientWrapper.cs
@@ -0,0 +1,9 @@
+﻿using ComputeNodeSwaggerClient;
+
+namespace Frontend.ComputeNodeSwaggerClient
+{
+    public interface IComputeNodeClientWrapper
+    {
+        Task<AtomicJobResult> RunAsync(int atomicJobId, int parentJobId, string inputData);
+    }
+}
