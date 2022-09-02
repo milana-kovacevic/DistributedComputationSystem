31b899679b6df7e7ddd166260d9790c2d3310205
milana-kovacevic <kovacevicmilana95@yahoo.com>
2022-09-02 22:38:44 +0200

    Fixing issue with data seeding from database - manually seeding AtomicJobs to Job object

--- src/Frontend/Models/SeedData.cs
+++ src/Frontend/Models/SeedData.cs
@@ -2,6 +2,9 @@
 using Frontend.Extensions;
 using Frontend.Managers;
 using Microsoft.EntityFrameworkCore;
+using NuGet.Packaging;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace Frontend.Models
 {
@@ -9,6 +12,8 @@ namespace Frontend.Models
     {
         public static void Initialize(IServiceProvider serviceProvider)
         {
+            IEnumerable<Job> activeJobs = new List<Job>();
+
             using (var context = new JobContext(serviceProvider.GetRequiredService<DbContextOptions<JobContext>>()))
             {
                 // Look for any job in a db.
@@ -16,11 +21,26 @@ namespace Frontend.Models
                 {
                     // Populate internally Job manager with active jobs from the db.
                     // This is needed to reschedule unfinished jobs in case of the service restart.
-                    var activeJobs = context.Job.AsEnumerable().Where(job => job.IsActive());
-                    var jobManager = serviceProvider.GetRequiredService<IJobManager>();
-                    jobManager.Initialize(activeJobs);
+                    activeJobs = context.Job.AsEnumerable().Where(job => job.IsActive());
 
-                    return;   // DB has been seeded
+                    if (activeJobs.Any() && context.AtomicJob.Any())
+                    {
+                        var activeAtomicJobs = context.AtomicJob.AsEnumerable().ToList();
+
+                        // TODO this should be automatically populated due to forgein key constraint when seeding Job object
+                        // BUG
+                        foreach (var job in activeJobs)
+                        {
+                            job.AtomicJobs ??= activeAtomicJobs.Where(atomicJob => atomicJob.JobId == job.Id).ToList();
+                            //job.AtomicJobs.AddRange(activeAtomicJobs.Where(atomicJob => atomicJob.JobId == job.Id));
+                        }
+
+                        var jobManager = serviceProvider.GetRequiredService<IJobManager>();
+                        jobManager.Initialize(activeJobs);
+
+                        return;   // DB has been seeded
+                    }
+
                 }
             }
         }
